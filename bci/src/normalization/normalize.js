bfb80259a92cef4c3634ba34451f315b20b93aed
atabeyler <info@boldkimya.com.tr>
2026-09-06 19:14:51 +0300

    BCI: wire advanced capability normalizers

--- bci/src/normalization/normalize.js
+++ bci/src/normalization/normalize.js
@@ -4,22 +4,16 @@ import { normalizeSemgrep } from './normalizers/semgrep.js';
 import { normalizeNuclei } from './normalizers/nuclei.js';
 import { normalizeNaabu } from './normalizers/naabu.js';
 import { normalizeHttpFuzz } from './normalizers/httpFuzz.js';
-
+import { normalizeIntrusiveValidation } from './normalizers/intrusiveValidation.js';
+import { normalizeAvailabilityProbe } from './normalizers/availabilityProbe.js';
 const NORMALIZERS = {
-  trivy: normalizeTrivy,
-  'osv-scanner': normalizeOsvScanner,
-  semgrep: normalizeSemgrep,
-  nuclei: normalizeNuclei,
-  naabu: normalizeNaabu,
-  'http-fuzz': normalizeHttpFuzz,
+  trivy: normalizeTrivy, 'osv-scanner': normalizeOsvScanner, semgrep: normalizeSemgrep,
+  nuclei: normalizeNuclei, naabu: normalizeNaabu, 'http-fuzz': normalizeHttpFuzz,
+  'intrusive-validation': normalizeIntrusiveValidation, 'availability-probe': normalizeAvailabilityProbe,
 };
-
 export function normalizeRaw(engineId, rawPayload) {
   const normalizer = NORMALIZERS[engineId];
   if (!normalizer) throw new Error(`No normalizer registered for engine "${engineId}"`);
   return normalizer(rawPayload);
 }
-
-export function supportsEngine(engineId) {
-  return Object.prototype.hasOwnProperty.call(NORMALIZERS, engineId);
-}
+export function supportsEngine(engineId) { return Object.prototype.hasOwnProperty.call(NORMALIZERS, engineId); }
